76194e7ab8c73e491a9d2bc144526d20f84c6e6a
mkarzel <karzelmarcin@gmail.com>
2020-04-20 15:50:03 +0200

    removed unnecesary create-react-app files and code

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const background = {
+  backgroundColor: 'black',
+  color: 'white',
+  minHeight: '100vh'
+}
+
+function App() {
+  return (
+    <div style={background}>
+    </div>
+  );
+}
+
+export default App;
