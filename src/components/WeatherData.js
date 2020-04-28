1b3bb65d7020eb133dfb43c23a0cf1d7b7c4fb1d
mkarzel <karzelmarcin@gmail.com>
2020-04-29 00:47:50 +0200

    add forecast display with styles

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -3,11 +3,19 @@ import axios from 'axios';
 import pressureIcon from '../images/atmospheric-pressure.png'
 import humidityIcon from '../images/humidity.png'
 import windIcon from '../images/wind-speed.png'
+import { Button, ButtonGroup } from '@material-ui/core';
 
 const mainContainer = {
     display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '40vh',
+}
+
+const measurementsContainer = {
+    display: 'flex',
     justifyContent: 'space-evenly',
-    padding: '2vw',
+    marginBottom: '2vw',
 }
 
 const measurementContainer = {
@@ -20,20 +28,35 @@ const measurementText = {
     fontSize: '2vw',
 }
 
+const buttonContainer = {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '1vw',
+    marginBottom: '1vw',
+}
+
+const buttonStyle = {
+    width: '10vw',
+    fontWeight: 'bold'
+}
+
 const iconSize = {
     width: '4vw'
 }
 
-const date = {
+const dateText = {
     fontSize: '2vw',
     color: 'yellow',
     textAlign: 'center'
 }
 
-const timestampToDate = (unixTimestamp) => {
-    const milliseconds = unixTimestamp * 1000   
-    const dateObject = new Date(milliseconds)   
-    return dateObject.toLocaleString() 
+const timestampToDate = (unixTimestamp, version) => {
+    const milliseconds = unixTimestamp * 1000
+    const dateObject = new Date(milliseconds)
+    if (version === 'short')
+        return dateObject.toLocaleString('pl-PL', { day: 'numeric', month: 'numeric', year: 'numeric' })
+    else
+        return dateObject.toLocaleString('pl-PL')
 }
 
 const key = `4c5fbd2fe1afe8b0cd155b79c2de7ef8`
@@ -44,6 +67,7 @@ const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${ln
 const WeatherData = () => {
 
     const [data, setData] = useState(null)
+    const [day, setDay] = useState(0)
 
     useEffect(() => {
         (async () => {
@@ -51,13 +75,18 @@ const WeatherData = () => {
             setData(response.data)
         })();
     }, []);
-    console.log(data)
 
     return (
         <div>
-            {data &&
-                (<div>
-                    <div style={mainContainer}>
+            {day === 0 && data &&
+                (<div style={mainContainer}>
+                    <div style={buttonContainer}>
+                        <Button style={buttonStyle} variant="contained" onClick={() => setDay(day + 1)}>TOMORROW</Button>
+                    </div>
+                    <div style={dateText}>
+                        <span>{timestampToDate(data.current.dt)}</span>
+                    </div>
+                    <div style={measurementsContainer}>
                         <div style={measurementContainer}>
                             <img style={iconSize} alt={`weather icon`} src={`http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`} />
                             <span style={measurementText}>{data.current.temp} &#8451;</span>
@@ -75,15 +104,48 @@ const WeatherData = () => {
                             <span style={measurementText}>{data.current.wind_speed} m/s</span>
                         </div>
                     </div>
-                    <div style={date}>
-                        <span>{timestampToDate(data.hourly[0].dt)}</span>
+                </div>
+                )
+            }
+            {day !== 0 && data &&
+                (<div style={mainContainer}>
+                    <div style={buttonContainer}>
+                        <ButtonGroup variant="contained">
+                            <Button style={buttonStyle} onClick={() => setDay(day - 1)}>PREVIOUS</Button>
+                            {day < data.daily.length - 1 ? <Button style={buttonStyle} onClick={() => setDay(day + 1)}>NEXT</Button> : null}
+                        </ButtonGroup>
+                    </div>
+                    <div style={dateText}>
+                        <span>{timestampToDate(data.daily[day].dt, 'short')}</span>
+                    </div>
+                    <div style={measurementsContainer}>
+                        <div style={measurementContainer}>
+                            <img style={iconSize} alt={`weather icon`} src={`http://openweathermap.org/img/w/${data.daily[day].weather[0].icon}.png`} />
+                            <div>
+                                <div style={measurementText}>morning: {data.daily[day].temp.morn} &#8451;</div>
+                                <div style={measurementText}>day: {data.daily[day].temp.day} &#8451;</div>
+                                <div style={measurementText}>evening: {data.daily[day].temp.eve} &#8451;</div>
+                                <div style={measurementText}>night: {data.daily[day].temp.night} &#8451;</div>
+                            </div>
+                        </div>
+                        <div style={measurementContainer}>
+                            <img style={iconSize} alt={`pressure icon`} src={pressureIcon} />
+                            <span style={measurementText}>{data.daily[day].pressure} hPa</span>
+                        </div>
+                        <div style={measurementContainer}>
+                            <img style={iconSize} alt={`humidity icon`} src={humidityIcon} />
+                            <span style={measurementText}>{data.daily[day].humidity} %</span>
+                        </div>
+                        <div style={measurementContainer}>
+                            <img style={iconSize} alt={`wind speed icon`} src={windIcon} />
+                            <span style={measurementText}>{data.daily[day].wind_speed} m/s</span>
+                        </div>
                     </div>
                 </div>
                 )
             }
         </div>
     );
-
 }
 
 export default WeatherData
