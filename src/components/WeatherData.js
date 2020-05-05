c291dc5c944ed6e91b8f16c24468ffa7a3d5791f
mkarzel <karzelmarcin@gmail.com>
2020-05-05 19:10:09 +0200

    renamed classes to BEM

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -6,13 +6,22 @@ import windIcon from '../images/wind-speed.png';
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles({
-    mainContainer: {
+    current: {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-evenly',
         minHeight: '50vh',
     },
-    currentDateText: {
+    current__measurements: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        paddingBottom: '2vw',
+    },
+    current__measurement: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    current__date: {
         color: 'yellow',
         textAlign: 'center',
         fontSize: '2vw',
@@ -20,49 +29,41 @@ const useStyles = makeStyles({
             fontSize: '3vh',
         }
     },
-    currentMeasurementsContainer: {
-        display: 'flex',
-        justifyContent: 'space-evenly',
-        paddingBottom: '2vw',
-    },
-    currentMeasurementContainer: {
-        display: 'flex',
-        alignItems: 'center',
-    },
-    currentMeasurementText: {
+    current__text: {
         paddingLeft: '1vw',
         fontSize: '2vw',
         '@media screen and (max-width: 600px)': {
             fontSize: '2vh',
         }
     },
-    currentIconSize: {
+    current__icon: {
         width: '3vw',
         '@media screen and (max-width: 600px)': {
             width: '3vh',
         }
     },
-    forecastContainer: {
+    forecast: {
         display: 'flex',
         justifyContent: 'space-evenly',
         '@media screen and (max-width: 600px)': {
             flexDirection: 'column'
         }
     },
-    forecastMeasurementsContainer: {
+    forecast__week: {
         '@media screen and (max-width: 600px)': {
             display: 'flex',
             justifyContent: 'space-evenly',
         }
     },
-    forecastText: {
-        paddingLeft: '0.5vw',
-        fontSize: '1.5vw',
+    forecast__day: {
+        display: 'flex',
+        paddingBottom: '1vh',
         '@media screen and (max-width: 600px)': {
-            fontSize: '1.5vh'
+            justifyContent: 'center',
+            width: '20vw',
         }
     },
-    forecastDateText: {
+    forecast__date: {
         display: 'flex',
         justifyContent: 'center',
         color: 'yellow',
@@ -72,21 +73,20 @@ const useStyles = makeStyles({
             width: '20vw',
         }
     },
-    forecastIconSize: {
+    forecast__text: {
+        paddingLeft: '0.5vw',
+        fontSize: '1.5vw',
+        '@media screen and (max-width: 600px)': {
+            fontSize: '1.5vh'
+        }
+    },
+    forecast__icon: {
         width: '2vw',
         '@media screen and (max-width: 600px)': {
             width: '2.5vh'
         }
     },
-    forecastDay: {
-        display: 'flex',
-        paddingBottom: '1vh',
-        '@media screen and (max-width: 600px)': {
-            justifyContent: 'center',
-            width: '20vw',
-        }
-    },
-    infoText: {
+    info__text: {
         fontSize: '3vw',
         color: 'yellow',
         textAlign: 'center',
@@ -144,25 +144,25 @@ const WeatherData = (props) => {
     if (data) {
         for (let i = 1; i < data.daily.length; i++) {
             forecast.push(
-                <div className={classes.forecastMeasurementsContainer} key={i}>
-                    <div className={classes.forecastDay}>
-                        <img className={classes.forecastIconSize} alt={`weather icon`} src={`http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`} />
-                        <span className={classes.forecastText}>{data.daily[i].temp.day}&#8451;</span>
+                <div className={classes.forecast__week} key={i}>
+                    <div className={classes.forecast__day}>
+                        <img className={classes.forecast__icon} alt={`weather icon`} src={`http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`} />
+                        <span className={classes.forecast__text}>{data.daily[i].temp.day}&#8451;</span>
                     </div>
-                    <div className={classes.forecastDay}>
-                        <img className={classes.forecastIconSize} alt={`pressure icon`} src={pressureIcon} />
-                        <span className={classes.forecastText}>{data.daily[i].pressure} hPa</span>
+                    <div className={classes.forecast__day}>
+                        <img className={classes.forecast__icon} alt={`pressure icon`} src={pressureIcon} />
+                        <span className={classes.forecast__text}>{data.daily[i].pressure} hPa</span>
                     </div>
-                    <div className={classes.forecastDay}>
-                        <img className={classes.forecastIconSize} alt={`humidity icon`} src={humidityIcon} />
-                        <span className={classes.forecastText}>{data.daily[i].humidity}%</span>
+                    <div className={classes.forecast__day}>
+                        <img className={classes.forecast__icon} alt={`humidity icon`} src={humidityIcon} />
+                        <span className={classes.forecast__text}>{data.daily[i].humidity}%</span>
                     </div>
-                    <div className={classes.forecastDay}>
-                        <img className={classes.forecastIconSize} alt={`wind speed icon`} src={windIcon} />
-                        <span className={classes.forecastText}>{data.daily[i].wind_speed} m/s</span>
+                    <div className={classes.forecast__day}>
+                        <img className={classes.forecast__icon} alt={`wind speed icon`} src={windIcon} />
+                        <span className={classes.forecast__text}>{data.daily[i].wind_speed} m/s</span>
                     </div>
                     <div>
-                        <div className={classes.forecastDateText}>{timestampToDate(data.daily[i].dt, 'short')}</div>
+                        <div className={classes.forecast__date}>{timestampToDate(data.daily[i].dt, 'short')}</div>
                     </div>
                 </div>)
         }
@@ -171,35 +171,35 @@ const WeatherData = (props) => {
     return (
         <div>
             {!data && (
-                <div className={classes.infoText}>Mark a place on the map to check the weather</div>
+                <div className={classes.info__text}>Mark a place on the map to check the weather</div>
             )
             }
             {data &&
-                (<div className={classes.mainContainer}>
+                (<div className={classes.current}>
                     <div>
-                        <div className={classes.currentDateText}>
+                        <div className={classes.current__date}>
                             <span>{timestampToDate(data.current.dt)}</span>
                         </div>
-                        <div className={classes.currentMeasurementsContainer}>
-                            <div className={classes.currentMeasurementContainer}>
-                                <img className={classes.currentIconSize} alt={`weather icon`} src={`http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`} />
-                                <span className={classes.currentMeasurementText}>{data.current.temp}&#8451;</span>
+                        <div className={classes.current__measurements}>
+                            <div className={classes.current__measurement}>
+                                <img className={classes.current__icon} alt={`weather icon`} src={`http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`} />
+                                <span className={classes.current__text}>{data.current.temp}&#8451;</span>
                             </div>
-                            <div className={classes.currentMeasurementContainer}>
-                                <img className={classes.currentIconSize} alt={`pressure icon`} src={pressureIcon} />
-                                <span className={classes.currentMeasurementText}>{data.current.pressure} hPa</span>
+                            <div className={classes.current__measurement}>
+                                <img className={classes.current__icon} alt={`pressure icon`} src={pressureIcon} />
+                                <span className={classes.current__text}>{data.current.pressure} hPa</span>
                             </div>
-                            <div className={classes.currentMeasurementContainer}>
-                                <img className={classes.currentIconSize} alt={`humidity icon`} src={humidityIcon} />
-                                <span className={classes.currentMeasurementText}>{data.current.humidity}%</span>
+                            <div className={classes.current__measurement}>
+                                <img className={classes.current__icon} alt={`humidity icon`} src={humidityIcon} />
+                                <span className={classes.current__text}>{data.current.humidity}%</span>
                             </div>
-                            <div className={classes.currentMeasurementContainer}>
-                                <img className={classes.currentIconSize} alt={`wind speed icon`} src={windIcon} />
-                                <span className={classes.currentMeasurementText}>{data.current.wind_speed} m/s</span>
+                            <div className={classes.current__measurement}>
+                                <img className={classes.current__icon} alt={`wind speed icon`} src={windIcon} />
+                                <span className={classes.current__text}>{data.current.wind_speed} m/s</span>
                             </div>
                         </div>
                     </div>
-                    <div className={classes.forecastContainer}>
+                    <div className={classes.forecast}>
                         {forecast}
                     </div>
                 </div>
