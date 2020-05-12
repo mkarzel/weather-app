4d798b036aa6a675b5295acb3e188dc2d86197ee
mkarzel <karzelmarcin@gmail.com>
2020-05-12 18:41:53 +0200

    Updates

---
+++ precache-manifest.e6bd847aed100ca6181014371f56e8e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b4d5d5388b1e8ed318b1eab88be945",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a5eb9e5663c4786bf663",
+    "url": "/weather-app/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "83febeb5e3661d467241",
+    "url": "/weather-app/static/css/main.0c0a77ae.chunk.css"
+  },
+  {
+    "revision": "a5eb9e5663c4786bf663",
+    "url": "/weather-app/static/js/2.8099b59c.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/weather-app/static/js/2.8099b59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83febeb5e3661d467241",
+    "url": "/weather-app/static/js/main.83e72a61.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  }
+]);
